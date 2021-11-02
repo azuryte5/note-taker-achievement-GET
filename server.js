c85641c99b63e834edc57eb4b4cafee37346dfa2
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-11-02 00:34:33 -0400

    Corrected req body not updating correctly

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
 const express = require('express');
-const { fstat } = require('fs');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 const { nanoid } = require('nanoid');
-
-//redundent somehow
-const notes = require('./db/db.json');
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -32,40 +28,25 @@ app.delete("/api/notes/:id", (req, res) =>{
 
 app.get('/api/notes', (req, res) => {
   let currentNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
-  // console.log("My Notes ..." + JSON.stringify(currentNotes));
-  console.log(currentNotes)
   res.json(currentNotes);
   });
 
-function makeNewNote (body, notesList){
-const note = body;
-note.id = nanoid();
-notesList.push(note);
-fs.writeFileSync(
-  path.join(__dirname, './db/db.json'),
-  JSON.stringify(notesList, null, 2)
-  );
-  return note;
-}
-
 app.post('/api/notes' , (req, res) => {
-  const note = makeNewNote(req.body, notes);
-
-  notes
-  console.log(note)
-  res.json(note);
+  const newNote = req.body
+  newNote.id = nanoid();
+  let currentNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+  currentNotes.push(newNote)
+  fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify(currentNotes, null, 2)
+    )
+  res.json(currentNotes);
 });
 
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/notes.html'))
   
 });
-
-// function findById(id, notesArray) {
-//   const result = notesArray.filter(notesArray.id === id)[0];
-//   return result;
-// } 
-
 
 app.get('/', (req,res) =>{
   res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -78,5 +59,4 @@ app.get('*', (req,res) =>{
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
-  // console.log(notes)
 });
